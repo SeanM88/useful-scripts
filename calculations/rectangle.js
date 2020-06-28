25633eb1608870fe4ef898a63fe82a506e8ad5f2
Sean Megan <sean@seanmegan.com>
2020-06-28 15:16:27 -0400

    Use explicit width and height params in static methods

--- calculations/rectangle.js
+++ calculations/rectangle.js
@@ -1,8 +1,8 @@
 class Rectangle {
   
-  constructor(rectangle = {}, width=null, height=null) {
-    this.width = rectangle.width ? rectangle.width : width;
-    this.height = rectangle.height ? rectangle.height : height;
+  constructor(width, height) {
+    this.width = parseFloat(width);
+    this.height = parseFloat(height);
   }
   
   get dimensions() {
@@ -10,37 +10,65 @@ class Rectangle {
   }
   
   set dimensions(sizes) {
-    [this.width, this.height] = sizes;
-  }
-  
-  get area() {
-    return Rectangle.calcArea(this.dimensions);
+    if (!Rectangle.isValidDimensions(sizes)) {
+      console.log('Dimensions failed to update, check input array for mistakes.');
+      return;
+    }
+    this.width = parseFloat(sizes[0]);
+    this.height = parseFloat(sizes[1]);
   }
   
   get diagonal() {
-    return Rectangle.calcDiagonalLength(this.dimensions);
+    return Rectangle.calcDiagonalLength(this.width, this.height);
   }
 
-  static calcArea(dimensions) {
-    if (!Array.isArray(dimensions) || dimensions.length !== 2) {
-      return undefined;
-    }
-    return parseFloat(dimensions[0]) * parseFloat(dimensions[1]);
+  get area() {
+    return Rectangle.calcArea(this.width, this.height);
   }
 
-  static calcDiagonalLength(dimensions) {
-    if (!Array.isArray(dimensions) || dimensions.length !== 2) {
-      return undefined;
-    }
+  get aspect_ratio() {
+    return Rectangle.calcAspectRatioFloat(this.width, this.height);
+  }
+
+  static calcArea(width, height) {
+    return parseFloat(width) * parseFloat(height);
+  }
+
+  static calcPerimeter(width, height) {
+    return 2 * (parseFloat(width) + parseFloat(height));
+  }
+
+  static calcDiagonalLength(width, height) {
     // Pythagorean theorem: c = sqrt(a^2 + b^2) on array containing sides a & b
-    return Math.sqrt( dimensions.reduce((acc, item) =>  acc + Math.pow(parseFloat(item), 2), 0) );
+    return Math.hypot(width, height);
   }
 
-  static calcAspectRatioFloat(dimensions) {
-    if (!Array.isArray(dimensions) || dimensions.length !== 2) {
-      return undefined;
+  static calcAspectRatioFloat(width, height) {
+    return parseFloat(width) / parseFloat(height);
+  }
+
+  static calcDimensionsFromDiagonalRatio(diag_len, aspect_ratio) {
+    const diag = parseFloat(diag_len), ratio = Number(aspect_ratio);
+    return {
+      width: diag / Math.sqrt((1 / Math.pow(ratio, 2)) + 1),
+      height: diag / Math.sqrt(Math.pow(ratio, 2) + 1),
+      diagonal: diag_len,
+      aspect_ratio: aspect_ratio
     }
-    return parseFloat(dimensions[0]) / parseFloat(dimensions[1]);
+  }
+
+  static calcHeightFromWidthRatio(width, aspect_ratio) {
+    // const width = parseFloat(width), ratio = Number(aspect_ratio);
+  }
+
+  static calcWidthFromHeightRatio(height, aspect_ratio) {
+    // const height = parseFloat(height), ratio = Number(aspect_ratio);
+  }
+
+  static isValidDimensions(dimensions) {
+    if (Array.isArray(dimensions) && dimensions.length === 2) {
+      return ( dimensions.filter(value => parseFloat(value) > 0).length === 2 ? true : false );
+    }
   }
 
 }
@@ -49,23 +77,11 @@ const box = new Rectangle(16,9);
 console.log(box.width); // Expect 16
 console.log(box.height); // Expect 9
 console.log(box.area); // Expect 144
-console.log(box.area); // Expect 144
-console.log(box.area); // Expect 144
-box.dimensions = [11.278,7.049];
+console.log(box.diagonal); // Expect 18.36
+console.log(box.aspect_ratio); // Expect 1.7778
+console.log(Rectangle.calcDimensionsFromDiagonalRatio(box.diagonal, box.aspect_ratio));
+box.dimensions = ['11.278"','7.049"'];
 console.log(box.width); // Expect 11.278
 console.log(box.height); // Expect 7.049
 console.log(box.area); // Expect 79.498622
-
-function findRectangleDimensions(diag_len, aspect_ratio) {
-  let width = diag_len / Math.sqrt((1 / Math.pow(aspect_ratio, 2)) + 1);
-  let height = diag_len / Math.sqrt(Math.pow(aspect_ratio, 2) + 1);
-  return {
-    diagonal: diag_len,
-    aspect_ratio: aspect_ratio,
-    width: width,
-    height: height,
-    area: width * height
-  }
-}
-
-
+console.log(box.diagonal); // Expect 13.3
